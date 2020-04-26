285cc0a71d118ef4ff1516db0e01f3f98304def4
Cubonacci <contact@cubonacci.com>
2020-04-26 14:42:03 +0000

    update data_loader

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from keras.datasets import mnist
+import numpy as np
 
 class DataLoader:
     def __init__(self):
@@ -7,13 +8,9 @@ class DataLoader:
 
     def load_training_data(self):
         # Load csv data file into dataframe
-        data = pd.read_csv("https://storage.googleapis.com/kaggle-data-sets/595/1134/bundle/archive.zip?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1587469169&Signature=WyGIaYmGjLUlncSpx3s5Vsbd4WgNEHsV3JN9WAi2%2F13g9voezm%2FYo0rjR%2BndwtXFunQAlLdcJ612BbG1AtVp9OI36ZatKc9tnVc4C0fQczuzENp8IDtjWH9basvLNpMIoHxOxyksHxf9T9NHN%2FOovjqZaBbN1GGiM76p8Uicd%2FUYV1wQXBfGrmDoTAThkGEB2w6apl6nukUckESe1KpVw31g5HU3CmngYYhU8ulZZv5m8kxD7R2sYy21QWIZ698a13ztEwlhnsWVXg%2F67gxmlPDpiKyK5K27gltlUa5qzzd19mRRjCvD9qLpiOWT12OuUSBoJHQaQcKLXO3pB0oR2Q%3D%3D&response-content-disposition=attachment%3B+filename%3Dsudoku.zip", compression='zip')
+        (X_train,y_train),(X_test,y_test) = mnist.load_data()
 
-        # Get number of features
-        feature_count = data.shape[1] - 1
-
-        # Split dataframe into X and y
-        X, y = data.iloc[:, :feature_count], data.iloc[:, feature_count:]
-        
+        X = np.concatenate((X_train, X_test))
+        y = np.concatenate((y_train, y_test)) 
 
         return X, y
