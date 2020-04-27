e722dfbecf9a2b33f3e0750ea16d6caabdd3e261
Marnix van den Broek <marnix.vandenbroek@cubonacci.com>
2020-04-27 13:31:30 +0200

    Update algorithm.py

--- algorithms/keras_cnn/algorithm.py
+++ algorithms/keras_cnn/algorithm.py
@@ -1,5 +1,6 @@
 class Algorithm:
-    def __init__(self):
+    def __init__(self, number_neurons_layer_1):
+        self.number_neurons_layer_1 = number_neurons_layer_1
         pass
 
     def predict(self, X):
